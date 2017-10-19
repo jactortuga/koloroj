ced6363d42d19417aad93630586c54478c926f1c
jactortuga <jac@tortugalabs.io>
2017-10-19 22:12:50 +0100

    es6 function implemented

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,13 @@ import styles from '../sass/main.sass';
 
 console.log('Hello World!');
 
+var generateColor = () => {
+  alert('I am about to generate a color');
+};
+
 
 document.body.onkeyup = function(e){
   if(e.keyCode === 32){
-    alert('ALERT');
+    generateColor();
   }
 };
