e4b21ab68c69eb9dbc2d3913603ba4790e59e8d0
jactortuga <jac@tortugalabs.io>
2017-10-19 22:21:52 +0100

    pseudocode for js functionality written

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,10 @@ const keyboardKey = 32;
 
 var generateColor = () => {
   alert('I am about to generate a color');
+
+  // Step 1: Pick random HEX value from color array
+  // Step 2: Remove selected HEX element from color array
+  // Step 3: Apply selected HEX value to background-color as inline style using transition
 };
 
 
