2af398741db3670a20a0606b1826ca550fc282c4
jactortuga <jac@tortugalabs.io>
2017-10-19 22:16:01 +0100

    if statement refactored in one liner

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ import styles from '../sass/main.sass';
 
 console.log('Hello World!');
 
+const keyboardKey = 32;
+
 var generateColor = () => {
   alert('I am about to generate a color');
 };
 
 
-document.body.onkeyup = function(e){
-  if(e.keyCode === 32){
-    generateColor();
-  }
+document.body.onkeyup = function(ev){
+  if (ev.keyCode === keyboardKey) generateColor();
 };
