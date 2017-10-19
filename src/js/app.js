5d792d215f8a7a2adc8ad08422f30b5d7b0dfa19
jactortuga <jac@tortugalabs.io>
2017-10-19 22:24:58 +0100

    js color change functionality init

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,12 @@ import styles from '../sass/main.sass';
 
 console.log('Hello World!');
 
-const keyboardKey = 32;
+const keyboardKey     = 32;
+const siteBackground  = document.body;
 
 var generateColor = () => {
-  alert('I am about to generate a color');
-
+  siteBackground.style.backgroundColor = '#117C57';
+  
   // Step 1: Pick random HEX value from color array
   // Step 2: Remove selected HEX element from color array
   // Step 3: Apply selected HEX value to background-color as inline style using transition
