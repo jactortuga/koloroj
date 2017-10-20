197f680ddcf40371eee3c73d50233bdbef70370a
jactortuga <jac@tortugalabs.io>
2017-10-20 21:27:51 +0100

    hex value code display functionality implemented

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import styles from '../sass/main.sass';
 
 const keyboardKey     = 32;
 const siteBackground  = document.body;
+const siteHexText     = document.getElementById('hex-value');
 const colorRange      = 'ABCDEF0123456789';
 
 
@@ -10,12 +11,16 @@ var generateColor = () => {
   let newHexValue = '#';
   for (let i = 0; i < 6; i++) newHexValue += colorRange.charAt(Math.floor(Math.random() * colorRange.length));
   updateBackgroundColor(newHexValue);
+  updateTextValue(newHexValue);
 };
 
 var updateBackgroundColor = (newColor) => {
   siteBackground.style.backgroundColor = newColor;
 };
 
+var updateTextValue = (newColor) => {
+  siteHexText.innerHTML = newColor;
+};
 
 document.body.onkeyup = function(ev){
   if (ev.keyCode === keyboardKey) generateColor();
