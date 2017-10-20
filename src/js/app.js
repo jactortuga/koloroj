e07a563578d1c49fb3f4f8bf53785931ca58195a
jactortuga <jac@tortugalabs.io>
2017-10-20 20:59:06 +0100

    background color functionality in new function

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,19 @@
 import styles from '../sass/main.sass';
 
+
 const keyboardKey     = 32;
 const siteBackground  = document.body;
 const colorRange      = 'ABCDEF0123456789';
 
+
 var generateColor = () => {
   let newHexValue = '#';
   for (let i = 0; i < 6; i++) newHexValue += colorRange.charAt(Math.floor(Math.random() * colorRange.length));
-  siteBackground.style.backgroundColor = newHexValue;
+  updateBackgroundColor(newHexValue);
+};
+
+var updateBackgroundColor = (newColor) => {
+  siteBackground.style.backgroundColor = newColor;
 };
 
 
