d9bd53cf8e09965b1096ea4bd593d23fd1d1f9eb
jactortuga <jac@tortugalabs.io>
2017-10-21 22:36:12 +0100

    menu functionality implemented

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,9 @@ import styles from '../sass/main.sass';
 const keyboardKey       = 32;
 const siteBackground    = document.body;
 const siteHexContainer  = document.getElementById('siteHexContainer');
+const menuButton        = document.getElementById('menuButton');
+const menuContainer     = document.getElementById('menuContainer');
 const colorRange        = 'ABCDEF0123456789';
-
 
 var generateColor = () => {
   let newHexValue = '#';
@@ -26,3 +27,25 @@ var updateTextValue = (newColor) => {
 document.body.onkeyup = function(ev){
   if (ev.keyCode === keyboardKey) generateColor();
 };
+
+menuButton.addEventListener('mouseenter', function(ev) {
+  menuContainer.classList.add('menu--reveal');
+});
+
+menuButton.addEventListener('mouseleave', function(ev) {
+  menuContainer.classList.remove('menu--reveal');
+});
+
+menuButton.addEventListener('keyup', function(ev) {
+  ev.preventDefault();
+});
+
+menuButton.addEventListener('click', function(ev) {
+  if(menuButton.classList.contains('button--close')) {
+    menuButton.classList.remove('button--close');
+    menuContainer.classList.remove('menu--show');
+  } else {
+    menuButton.classList.add('button--close');
+    menuContainer.classList.add('menu--show');
+  }
+});
